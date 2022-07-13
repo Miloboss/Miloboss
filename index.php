8f453a604bbae22abbe96e5e8bb937fdee763e7c
Miloboss <90743389+Miloboss@users.noreply.github.com>
2022-07-13 13:05:54 +0630

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <title>ThinZar BookShop::Contact && Feedback</title>
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="style.css">
 
     <style>
        @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
